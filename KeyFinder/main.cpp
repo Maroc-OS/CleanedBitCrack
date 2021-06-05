0ab981a449438735ad85a67aa0e8dbd855b611b7
Yahya Lmallas <1588632+Maroc-OS@users.noreply.github.com>
2021-06-05 03:02:09 +0100

    sensible default params from L0laapk3 /L0laapk3/BitCrack/commit/47b90e11b81623a11d1f3287df767e4df364de04 Signed-off-by: Luca Bognolo <git@bogny.eu>

--- KeyFinder/main.cpp
+++ KeyFinder/main.cpp
@@ -339,9 +339,9 @@ typedef struct {
 DeviceParameters getDefaultParameters(const DeviceManager::DeviceInfo &device);
 DeviceParameters getDefaultParameters(const DeviceManager::DeviceInfo &device) {
 	DeviceParameters p = {0,0,0};
-	p.threads = 256;
-	p.blocks = 32;
-	p.pointsPerThread = 32;
+	p.threads = 32;
+	p.blocks = device.computeUnits * p.threads;
+	p.pointsPerThread = pow(2, floor(log(device.memory / (4 * 1024 * p.blocks)) / log(2))); //best power of 2 based on device memory assuming 4kb per block.
 
 	return p;
 }
